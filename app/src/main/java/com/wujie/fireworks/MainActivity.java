697dceacd37c2164355957575815cd0eecaa9fb2
Blowing <1072878103@qq.com>
2018-04-16 13:35:51 +0800

    Initial commit

---
+++ app/src/main/java/com/wujie/fireworks/MainActivity.java
@@ -0,0 +1,21 @@
+package com.wujie.fireworks;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings wSet = webView.getSettings();
+        //调用WebView关联的WebSettings中setJavaScriptEnable(true)方法。
+        wSet.setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/web/index.html");
+    }
+}
