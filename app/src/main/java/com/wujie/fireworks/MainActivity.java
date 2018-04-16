ae73702fce11bfb5cb67f6da9bbee521d6963d6d
Blowing <1072878103@qq.com>
2018-04-16 14:28:21 +0800

    添加上文字效果

--- app/src/main/java/com/wujie/fireworks/MainActivity.java
+++ app/src/main/java/com/wujie/fireworks/MainActivity.java
@@ -1,5 +1,6 @@
 package com.wujie.fireworks;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebSettings;
@@ -12,7 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
         webView = (WebView) findViewById(R.id.webView);
+
         WebSettings wSet = webView.getSettings();
         //调用WebView关联的WebSettings中setJavaScriptEnable(true)方法。
         wSet.setJavaScriptEnabled(true);
